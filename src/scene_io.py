03db4e2db4dd16b98c6f0732b0b8c0ca5ef596f2
miili <isken@gfz-potsdam.de>
2016-08-19 12:23:32 +0200

    Implemented support Gamma IO

--- src/scene_io.py
+++ src/scene_io.py
@@ -21,7 +21,7 @@ class SceneIO(object):
         :param **kwargs: Keyword arguments
         :type **kwargs: {dict}
         """
-        pass
+        raise NotImplementedError('read not implemented')
 
     def write(self, filename, **kwargs):
         """Write method for IO
@@ -31,7 +31,7 @@ class SceneIO(object):
         :param **kwargs: Keyword arguments
         :type **kwargs: {dict}
         """
-        pass
+        raise NotImplementedError('write not implemented')
 
     def validate(self, filename, **kwargs):
         """Validate file format
@@ -42,12 +42,12 @@ class SceneIO(object):
         :rtype: {bool}
         """
         pass
-        return False
+        raise NotImplementedError('validate not implemented')
 
 
-class MatlabData(SceneIO):
+class Matlab(SceneIO):
     """Reads Matlab .mat files into :py:class:`kite.scene.Scene`
-    
+
     Variable naming convenctions in .mat variables
     ============ ==================
     Property     .mat name contains
@@ -87,6 +87,98 @@ class MatlabData(SceneIO):
         return self.data_items
 
 
+class Gamma(SceneIO):
+    """Reads Gamma binary files
+
+    [description]
+    """
+    def _getParameterFile(self, filename):
+        import os
+        import glob
+
+        path = os.path.dirname(os.path.realpath(filename))
+        try:
+            return glob.glob('%s/*.gc_par' % path)[0]
+        except ValueError:
+            raise ImportError('Could not find Gamma parameter file (.gc_par)')
+
+    def _parseParameterFile(self, parameter_file):
+        import re
+
+        parameters = {}
+        rc = re.compile(r'^(\w*):\s*([a-zA-Z0-9+-.*]*\s[a-zA-Z0-9_]*).*')
+
+        with open(parameter_file, mode='r') as par:
+            for line in par:
+                parsed = rc.match(line)
+                if parsed is None:
+                    continue
+
+                groups = parsed.groups()
+                try:
+                    parameters[groups[0]] = float(groups[1])
+                except ValueError:
+                    parameters[groups[0]] = groups[1].strip()
+        return parameters or None
+
+    def validate(self, filename, parameter_file=None):
+        if parameter_file is None:
+            parameter_file = self._getParameterFile(filename)
+        par = self._parseParameterFile(parameter_file)
+
+        if par is None:
+            return False
+            raise ValueError('Parameter file %s is empty' % parameter_file)
+
+        return True
+
+    def read(self, filename, parameter_file=None):
+        import numpy as num
+        import utm
+
+        if parameter_file is None:
+            parameter_file = self._getParameterFile(filename)
+        par = self._parseParameterFile(parameter_file)
+
+        try:
+            par['width'] = int(par['width'])
+            par['nlines'] = int(par['nlines'])
+        except:
+            raise ValueError('Error parsing width and nlines from %s' %
+                             parameter_file)
+
+        self.data_items['displacement'] = num.fromfile(filename, dtype='>f4')
+
+        # Resize array last line is not scanned completely
+        _fill = num.empty(par['width'] -
+                          self.data_items['displacement'].size % par['width'])
+        _fill.fill(num.nan)
+        self.data_items['displacement'] = num.append(
+                          self.data_items['displacement'], _fill)
+
+        # Cast vector
+        self.data_items['displacement'] = \
+            self.data_items['displacement'].reshape(par['nlines'],
+                                                    par['width'])
+
+        # LatLon UTM Conversion
+        utm_x, utm_y, utm_zone, _ = utm.from_latlon(par['corner_lat'],
+                                                    par['corner_lon'])
+        last_x, last_y, _, _ = utm.from_latlon(
+                            par['corner_lat'] + par['post_lat']*par['width'],
+                            par['corner_lon'] + par['post_lon']*par['nlines'])
+        self.data_items['utm_x'] = num.linspace(utm_x, last_x, par['width'])
+        self.data_items['utm_y'] = num.linspace(utm_y, last_y, par['nlines'])
+
+        # Theta and Phi
+        self.data_items['theta'] = 0.
+        self.data_items['phi'] = 0.
+        # raise DeprecationWarning('Set Scene.phi and Scene.theta manually!')
+
+        return self.data_items
+
+
 __all__ = """
-MatlabData
+Matlab
+Gamma
 """.split()
