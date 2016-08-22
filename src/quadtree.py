664c55c4ae42449bd0d68f69c94adc4a7f69d087
miili <isken@gfz-potsdam.de>
2016-08-22 12:00:20 +0200

    Now pre-calculating the tree to certain limit

--- src/quadtree.py
+++ src/quadtree.py
@@ -23,8 +23,8 @@ class QuadNode(object):
         self.lly = int(lly)
         self.length = int(length)
 
-        self.data = self._tree.data[self.llx:self.llx+self.length,
-                                    self.lly:self.lly+self.length]
+        self.data = self._tree._data[self.llx:self.llx+self.length,
+                                     self.lly:self.lly+self.length]
 
     @property_cached
     def nan_fraction(self):
@@ -76,6 +76,14 @@ class QuadNode(object):
                 for q in c.iterLeafs():
                     yield q
 
+    def iterLeafsEval(self, eval_func):
+        if eval_func(self) < self._tree.epsilon or self.children is None:
+            yield self
+        else:
+            for c in self.children:
+                for q in c.iterLeafsEval(eval_func):
+                    yield q
+
     def _iterSplitNode(self):
         if self.length == 1:
             yield None
@@ -88,13 +96,13 @@ class QuadNode(object):
                 continue
             yield _q
 
-    def evaluateNode(self, eval_func):
-        if eval_func(self) > self._tree.epsilon:  # or\
-            # self.length > .1 * max(self._tree.data.shape): !! Very Expensive
+    def createTree(self, eval_func):
+        if eval_func(self) > self._tree._epsilon_limit:  # or\
+            # self.length > .1 * max(self._tree._data.shape): !! Very Expensive
             if self._children is None:
                 self._children = [c for c in self._iterSplitNode()]
             for c in self._children:
-                c.evaluateNode(eval_func)
+                c.createTree(eval_func)
         else:
             self._children = None
 
@@ -115,19 +123,18 @@ class Quadtree(Subject):
     def __init__(self, scene, epsilon=None):
         Subject.__init__(self)
 
-        self.split_methods = {
+        self._split_methods = {
             'mean_std': lambda node: node.mean_std,
             'median_std': lambda node: node.median_std,
             'std': lambda node: node.std,
         }
-
-        self.norm_methods = {
+        self._norm_methods = {
             'mean': lambda node: node.mean,
             'median': lambda node: node.median,
         }
 
         self._scene = scene
-        self.data = self._scene.displacement
+        self._data = self._scene.displacement
 
         self._epsilon = None
         self._leafs = None
@@ -136,28 +143,35 @@ class Quadtree(Subject):
 
         self.setSplitMethod('median_std')
 
-    def setSplitMethod(self, method):
+    def setSplitMethod(self, split_method):
         """Set splitting method for quadtree tiles
 
         * `mean_std` tiles standard deviation from tile's mean is evaluated
         * `median_std` tiles standard deviation from tile's median is evaluated
         * `std` tiles standard deviation is evaluated
 
-        :param method: Choose from methods `['mean_std', 'median_std', 'std']`
-        :type method: string
+        :param split_method: Choose from methods
+                             `['mean_std', 'median_std', 'std']`
+        :type split_method: string
         :raises: AttributeError
         """
-        if method not in self.split_methods.keys():
+        if split_method not in self._split_methods.keys():
             raise AttributeError('Method %s not in %s'
-                                 % (method, self.split_methods))
-        self._log.info('Changing to discretesiation method %s' % method)
-        self.method = method
-        self.epsilon = self._init_epsilon
+                                 % (split_method, self._split_methods))
+
+        self._log.info('Creating tree using %s method...' % split_method)
+        self.split_method = split_method
+        self._split_func = self._split_methods[split_method]
+
+        self._epsilon_limit = self._epsilon_init * .1
+        self.epsilon = self._epsilon_init
+
+        for b in self._base_nodes:
+            b.createTree(self._split_func)
 
     @property
-    def _init_epsilon(self):
-        return num.mean([self.split_methods[self.method](b)
-                         for b in self._base_nodes])
+    def _epsilon_init(self):
+        return num.mean([self._split_func(b) for b in self._base_nodes])
 
     @property
     def epsilon(self):
@@ -165,29 +179,23 @@ class Quadtree(Subject):
 
     @epsilon.setter
     def epsilon(self, value):
-        if value < 0.1 * self._init_epsilon:
+        if value < self._epsilon_limit:
             self._log.info('Epsilon is out of bounds [%0.3f]' % value)
             return
+        self.leafs = None
         self._epsilon = value
-        self._leafs = None
-        self._log.info('Changing epsilon to %0.3f' % value)
-        t0 = time.time()
-
-        for b in self._base_nodes:
-            b.evaluateNode(self.split_methods[self.method])
-
-        self._log.info('New tree has %d leafs [%0.8f s]' %
-                       (len(self.leafs), (time.time()-t0)))
         self._notify()
         return
 
-    @property
+    @property_cached
     def leafs(self):
-        if self._leafs is None:
-            self._leafs = []
-            for n in self._base_nodes:
-                self._leafs.extend([c for c in n.iterLeafs()])
-        return self._leafs
+        t0 = time.time()
+        leafs = []
+        for n in self._base_nodes:
+            leafs.extend([c for c in n.iterLeafsEval(self._split_func)])
+        self._log.info('Gathering leafs for epsilon %.3f [%0.8f s]' %
+                       (self.epsilon, time.time()-t0))
+        return leafs
 
     @property
     def leaf_means(self):
@@ -202,31 +210,32 @@ class Quadtree(Subject):
         return num.array([n.focal_point for n in self.leafs])
 
     @property
-    def leaf_matrix_mean(self):
+    def leaf_matrix_means(self):
         return self._getLeafsNormMatrix(method='mean')
 
     @property
-    def leaf_matrix_median(self):
+    def leaf_matrix_medians(self):
         return self._getLeafsNormMatrix(method='median')
 
     def _getLeafsNormMatrix(self, method='median'):
-        if method not in self.norm_methods.keys():
+        if method not in self._norm_methods.keys():
             raise AttributeError('Method %s is not in %s' % (method,
-                                 self.norm_methods.keys()))
+                                 self._norm_methods.keys()))
 
-        leaf_matrix = num.empty_like(self.data)
+        leaf_matrix = num.empty_like(self._data)
         leaf_matrix.fill(num.nan)
         for l in self.leafs:
             leaf_matrix[l.llx:l.llx+l.length, l.lly:l.lly+l.length] = \
-                self.norm_methods[method](l)
+                self._norm_methods[method](l)
         return leaf_matrix
 
     @property_cached
     def _base_nodes(self):
         self._base_nodes = []
-        init_length = num.power(2, num.ceil(num.log(num.min(self.data.shape)) /
-                                            num.log(2)))
-        nx, ny = num.ceil(num.array(self.data.shape)/init_length)
+        init_length = num.power(2,
+                                num.ceil(num.log(num.min(self._data.shape)) /
+                                         num.log(2)))
+        nx, ny = num.ceil(num.array(self._data.shape)/init_length)
 
         for ix in range(int(nx)):
             for iy in range(int(ny)):
@@ -247,12 +256,12 @@ class Quadtree(Subject):
     def __str__(self):
         return '''
 Quadtree for %s
-  Initiated: %s
-  Epsilon: %0.3f
-  nLeafs:  %d
-  Method: %s
+  initiated: %s
+  epsilon: %0.3f
+  nleafs: %d
+  split_method: %s
         ''' % (repr(self._scene), (self._base_nodes is not None),
-               self.epsilon, len(self.leafs), self.method)
+               self.epsilon, len(self.leafs), self.split_method)
 
 __all__ = '''
 Quadtree
