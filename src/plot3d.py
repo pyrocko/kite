2c09769b48994008bc5b464d998c75ee0ac533fa
miili <isken@gfz-potsdam.de>
2016-08-25 12:37:27 +0200

    Sketching vtk objects

--- src/plot3d.py
+++ src/plot3d.py
@@ -1,25 +1,29 @@
-from vtk import vtkPolyData, vtkDelaunay2D
+from vtk import vtkPolyData, vtkDelaunay3D
 from vtk.util import numpy_support
 import numpy as num
 
 
+class vtkSceneDisplacement(vtkPolyData):
+    pass
+
+
+class vtkSceneDEM(vtkPolyData):
+    pass
+
+
+class _vtkSceneDelaunay3D(vtkDelaunay3D):
+    pass
+
+
+class vtkSceneDisplacementDelaunay(_vtkSceneDelaunay3D):
+    pass
+
+
+class vtkSceneDEMDelaunay(_vtkSceneDelaunay3D):
+    pass
+
+
 class Plot3D(object):
-
-    class vtkSceneDisplacement(vtkPolyData):
-        pass
-
-    class vtkSceneDEM(vtkPolyData):
-        pass
-
-    class _vtkSceneDelaunay2D(vtkDelaunay2D):
-        pass
-
-    class vtkSceneDisplacementDelaunay(_vtkSceneDelaunay2D):
-        pass
-
-    class vtkSceneDEMDelaunay(_vtkSceneDelaunay2D):
-        pass
-
     def __init__(self, scene):
         self.scene = scene
 
