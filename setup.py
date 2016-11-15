1b159a6160f105e6634947d14f606a7729e88f73
miili <isken@gfz-potsdam.de>
2016-11-15 10:54:29 +0100

    Upgraded setup.py // pythonpy conform // added c extension covariance_ext.c

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
 #!/bin/python
-from setuptools import setup
+from setuptools import setup, Extension
+from os.path import join as pjoin
+try:
+    import numpy
+except ImportError:
+    class numpy():
+        def __init__(self):
+            pass
+
+        @classmethod
+        def get_include(self):
+            return ''
 import time
 
 setup(
     name='kite',
     version='0.0.2-%s' % time.strftime('%Y%m%d'),
     description='Handle SAR displacement data towards pyrocko',
-    author='Marius P. Isken',
+    author='Marius P. Isken, Henriette Sudhaus;'
+           'BRiDGES Emmily Noether-Programm (DFG)',
     author_email='misken@geophysik.uni-kiel.de',
+
+    license='GPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: C',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        ],
+    keywords=['insar satellite radar earthquake optimization'],
+
     install_requires=['numpy>=1.9.0', 'pyrocko', 'scipy', 'pyyaml',
                       'progressbar'],
     packages=['kite', 'kite.spool'],
@@ -15,5 +45,23 @@ setup(
     data_files=[('kite/spool/ui/', ['src/spool/ui/spool.ui'])],
     entry_points={
         'console_scripts': ['spool = kite.spool.__main__:main'],
-    }
+    },
+
+    ext_modules=[
+        Extension('covariance_ext',
+                  sources=[pjoin('src', 'covariance_ext.c')],
+                  include_dirs=[numpy.get_include()],
+                  define_macros=None,
+                  undef_macros=None,
+                  library_dirs=None,
+                  libraries=None,
+                  runtime_library_dirs=None,
+                  extra_objects=None,
+                  extra_compile_args=None,
+                  extra_link_args=None,
+                  export_symbols=None,
+                  swig_opts=None,
+                  depends=None,
+                  language=None)
+    ]
 )
