f426b3d437795d30abbf462f164f09615cab18c8
miili <isken@gfz-potsdam.de>
2016-11-16 19:42:08 +0100

    Adding dedicated covariance test

---
+++ tests/test_covariance.py
@@ -0,0 +1,57 @@
+#!/bin/python
+import unittest
+import numpy as num
+from common import Benchmark
+from kite import Scene, SceneTest
+import matplotlib.pyplot as plt
+import os
+
+benchmark = Benchmark()
+
+
+class TestCovariance(unittest.TestCase):
+
+    def setUp(self):
+        file = os.path.join(
+         os.path.abspath(os.path.dirname(__file__)),
+         'data/20110214_20110401_ml4_sm.unw.geo_ig_dsc_ionnocorr.mat')
+
+        self.sc = Scene.import_file(file)
+        self.sc.meta.scene_title = 'Matlab Input - Myanmar 2011-02-14'
+        
+        # self.sc = SceneTest.createGauss(ny=250)
+
+    def __setUp(self):
+        self.sc = SceneTest.createGauss()
+        # self.sc._log.setLevel('CRITICAL')
+
+    def _testCovariance(self):
+        self.sc.quadtree.epsilon = .02
+        self.sc.quadtree.covariance.subsampling = 24
+        cov = self.sc.quadtree.covariance
+
+        matrix_focal = cov.covariance_matrix
+        matrix = cov.covariance_matrix_focal
+        num.testing.assert_allclose(matrix, matrix_focal,
+                                    rtol=1e-7, atol=2e3, verbose=True)
+
+    @benchmark
+    def testCovarianceVisual(self):
+        self.sc.quadtree.epsilon = .02
+        cov = self.sc.quadtree.covariance
+        l = self.sc.quadtree.leafs[0]
+        d = []
+        # d.append(('Matrix - Pool', cov._calcDistanceMatrix(method='matrix')))
+        d.append(('Matrix - C', cov._calcDistanceMatrix(method='matrix_c')))
+        d.append(('Matrix - Focal', cov._calcDistanceMatrix(method='focal')))
+
+        fig, _ = plt.subplots(1, len(d))
+        for i, (title, mat) in enumerate(d):
+            print '%s Max %f' % (title, num.nanmax(mat)), mat.shape
+            fig.axes[i].imshow(mat)
+            fig.axes[i].set_title(title)
+        plt.show()
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+    # print benchmark
