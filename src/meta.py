42b3fc8086a90fd160f17cb99c198a8d238197b6
miili <isken@gfz-potsdam.de>
2016-12-06 14:39:33 +0100

    Added alternative deramping functions

--- src/meta.py
+++ src/meta.py
@@ -29,6 +29,85 @@ def derampMatrix(displ):
     return displ - rx[num.newaxis, :] - ry[:, num.newaxis]
 
 
+def derampMatrix2(displ):
+    """ Deramp through fitting a bilinear plane
+    using scipy.stats.linregress
+    """
+    if displ.ndim != 2:
+        raise TypeError('Displacement has to be 2-dim array')
+
+    c_grid = num.mgrid[0:displ.shape[0], 0:displ.shape[1]]
+
+    # separate and flatten coordinate grid into x and y vectors for each !point
+    ix = c_grid[0].flatten()
+    iy = c_grid[1].flatten()
+    displ_f = displ.flatten()
+
+    # reduce vectors taking out all NaN's
+    displ_nonan = displ_f[num.isfinite(displ_f)]
+    ix = ix[num.isfinite(displ_f)]
+    iy = iy[num.isfinite(displ_f)]
+
+    dx, cx, _, _, _ = sp.stats.linregress(ix, displ_nonan)
+    dy, cy, _, _, _ = sp.stats.linregress(iy, displ_nonan)
+
+    rx = (ix * dx + cx)
+    ry = (iy * dy + cy)
+
+    ramp_x = num.multiply(displ_f, 0.)
+    ramp_y = num.multiply(displ_f, 0.)
+    # insert ramp values in full vectors
+    num.place(ramp_x, num.isfinite(displ_f), num.array(rx).flatten())
+    num.place(ramp_y, num.isfinite(displ_f), num.array(ry).flatten())
+    ramp_x = ramp_x.reshape(*displ.shape)
+    ramp_y = ramp_y.reshape(*displ.shape)
+
+    return displ - ramp_x - ramp_y
+
+
+def derampGMatrix(displ):
+    """ Deramp through lsq a bilinear plane
+    Data is also de-meaned
+    """
+    if displ.ndim != 2:
+        raise TypeError('Displacement has to be 2-dim array')
+
+    # form a relative coordinate grid
+    c_grid = num.mgrid[0:displ.shape[0], 0:displ.shape[1]]
+
+    # separate and flatten coordinate grid into x and y vectors for each !point
+    ix = c_grid[0].flat
+    iy = c_grid[1].flat
+    displ_f = displ.flat
+
+    # reduce vectors taking out all NaN's
+    displ_nonan = displ_f[num.isfinite(displ_f)]
+    ix = ix[num.isfinite(displ_f)]
+    iy = iy[num.isfinite(displ_f)]
+
+    # form kernel/design derampMatrix (c, x, y)
+    GT = num.matrix([num.ones(len(ix)), ix, iy])
+    G = GT.T
+
+    # generalized kernel matrix (quadtratic)
+    GTG = GT * G
+    # generalized inverse
+    GTGinv = GTG.I
+
+    # lsq estimates of ramp parameter
+    ramp_paras = displ_nonan * (GTGinv * GT).T
+
+    # ramp values
+    ramp_nonan = ramp_paras * GT
+    ramp_f = num.multiply(displ_f, 0.)
+
+    # insert ramp values in full vectors
+    num.place(ramp_f, num.isfinite(displ_f), num.array(ramp_nonan).flatten())
+    ramp_f = ramp_f.reshape(displ.shape[0], displ.shape[1])
+
+    return displ - ramp_f
+
+
 def trimMatrix(displ):
     """Trim displacement matrix from all NaN rows and columns
     """
