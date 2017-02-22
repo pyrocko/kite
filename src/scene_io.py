464378a2ea382c7ba73431aaed37948dc466b2e3
miili <isken@gfz-potsdam.de>
2017-02-22 13:56:07 +0100

    Parsing frequencies for IO

--- src/scene_io.py
+++ src/scene_io.py
@@ -46,6 +46,7 @@ class SceneIO(object):
                 'title': None,
                 'orbit_direction': None,
                 'satellite_name': None,
+                'wavelength': None,
                 'time_master': None,
                 'time_slave': None
             },
@@ -171,10 +172,12 @@ class Gamma(SceneIO):
     Expects two files:
 
         * Binary file from Gamma (:file:`*`)
-        * Parameter file (:file:`*par`) describing ``corner_lat, corner_lon,
-          nlines, width, post_lat, post_lon``
-          or ``'post_north', 'post_east', 'corner_east',
-          'corner_north', 'nlines', 'width'``
+        * Parameter file (:file:`*par`) describing `corner_lat, corner_lon,
+          nlines, width, post_lat, post_lon`
+          or `'post_north', 'post_east', 'corner_east',
+          'corner_north', 'nlines', 'width'`
+        * when `radar_frequency` in [Hz] is given in the :file:`*.par` file,
+          the displacement is expected to be in radians and will be scaled.
     """
     def _getParameterFile(self, path):
         path = os.path.dirname(os.path.realpath(path))
@@ -239,7 +242,7 @@ class Gamma(SceneIO):
             return 0.
 
         filename = phi_files[0]
-        self._log.debug('Found %s in %s' % (pattern, filename))
+        self._log.info('Found %s in %s' % (pattern, filename))
         return num.memmap(filename, mode='r', dtype='>f4')
 
     def read(self, filename, **kwargs):
@@ -260,6 +263,7 @@ class Gamma(SceneIO):
 
         nrows = int(par['width'])
         nlines = int(par['nlines'])
+        radar_frequency = par.get('radar_frequency', None)
 
         displ = num.fromfile(filename, dtype='>f4')
         # Resize array if last line is not scanned completely
@@ -275,26 +279,72 @@ class Gamma(SceneIO):
         theta = self._getAngle(filename, '*theta*')
         theta = num.cos(theta)
 
+        if isinstance(phi, num.ndarray):
+            phi.reshape(nlines, nrows)
+        if isinstance(theta, num.ndarray):
+            theta.reshape(nlines, nrows)
+
         if fill is not None:
             theta = num.append(theta, fill)
             phi = num.append(phi, fill)
 
-        theta = theta.reshape(nlines, nrows)
-        phi = phi.reshape(nlines, nrows)
-
-        # LatLon UTM Conversion
         c = self.container
-        c['displacement'] = displ*1e-2
-        c['frame']['llLat'] = par['corner_lat'] + par['post_lat'] * nrows
-        c['frame']['llLon'] = par['corner_lon']
-        c['frame']['dLon'] = par['post_lon']
-        c['frame']['dLat'] = par['post_lat']
+
+        if radar_frequency is not None:
+            self._log.info('Scaling radian displacement by radar_frequency')
+            wavelength = 299792458. / radar_frequency
+            displ = (displ / (4.*num.pi)) * wavelength
+            c['meta']['wavelength'] = wavelength
+
+        c['displacement'] = displ
         c['theta'] = theta
         c['phi'] = phi
 
-        c['meta']['title'] = par.get('title', None)
+        c['meta']['title'] = par.get('title', 'None')
         c['bin_file'] = filename
         c['par_file'] = par_file
+
+        if par['DEM_projection'] == 'UTM':
+            self._log.info('Parameter file provides UTM reference')
+            import utm
+
+            utm_zone = par['projection_zone']
+            try:
+                utm_zone_letter = utm.latitude_to_zone_letter(
+                    par['center_latitude'])
+            except ValueError:
+                self._log.warning('Could not parse UTM Zone letter,'
+                                  ' defaulting to N!')
+                utm_zone_letter = 'N'
+
+            utm_e = utm_n = None
+            dN = par['post_north']
+            dE = par['post_east']
+            utm_corn_e = par['corner_east']
+            utm_corn_n = par['corner_north']
+
+            utm_corn_eo = utm_corn_e + dE * displ.shape[1]
+            utm_corn_no = utm_corn_n + dN * displ.shape[0]
+
+            utm_e = num.linspace(utm_corn_e, utm_corn_eo, displ.shape[1])
+            utm_n = num.linspace(utm_corn_n, utm_corn_no, displ.shape[0])
+
+            c['frame']['llLat'], c['frame']['llLon'] =\
+                utm.to_latlon(utm_e.min(), utm_n.min(),
+                              utm_zone, utm_zone_letter)
+            urlat, urlon = utm.to_latlon(utm_e.max(), utm_n.max(),
+                                         utm_zone, utm_zone_letter)
+            c['frame']['dLat'] =\
+                (urlat - c['frame']['llLat']) / displ.shape[0]
+
+            c['frame']['dLon'] =\
+                (urlon - c['frame']['llLon']) / displ.shape[1]
+        else:
+            self._log.info('Parameter file provides Lat/Lon reference')
+            c['frame']['llLat'] = par['corner_lat'] + par['post_lat'] * nrows
+            c['frame']['llLon'] = par['corner_lon']
+            c['frame']['dLon'] = par['post_lon']
+            c['frame']['dLat'] = par['post_lat']
         return self.container
 
 
@@ -302,12 +352,15 @@ class ROI_PAC(SceneIO):
     """
     .. warning :: Data has to be georeferenced to latitude/longitude!
 
+    The unwrapped displacement is expected in radians and will be scaled by
+    `WAVELENGTH` parsed from the :file:`*.rsc` file.
+
     Expects two files:
 
         * Binary file from ROI_PAC (:file:`*`)
         * Parameter file (:file:`<binary_file>.rsc`)
           describing ``'WIDTH', 'FILE_LENGTH', 'X_FIRST', 'Y_FIRST', 'X_STEP',
-          'Y_STEP'``
+          'Y_STEP', 'WAVELENGTH``
     """
 
     def validate(self, filename, **kwargs):
@@ -335,7 +388,7 @@ class ROI_PAC(SceneIO):
 
         params = {}
         required = ['WIDTH', 'FILE_LENGTH', 'X_FIRST', 'Y_FIRST', 'X_STEP',
-                    'Y_STEP']
+                    'Y_STEP', 'WAVELENGTH']
 
         rc = re.compile(r'([\w]*)\s*([\w.+-]*)')
         with open(par_file, 'r') as par:
@@ -370,12 +423,14 @@ class ROI_PAC(SceneIO):
 
         nlines = int(par['FILE_LENGTH'])
         nrows = int(par['WIDTH'])
+        wavelength = par['WAVELENGTH']
 
         data = num.memmap(filename, dtype='<f4')
         data = data.reshape(nlines, nrows*2)
 
         displ = data[:, nrows:]
         displ[displ == -0.] = num.nan
+        displ = displ / (4.*num.pi) * wavelength
 
         z_scale = par.get('Z_SCALE', 1.)
         z_offset = par.get('Z_OFFSET', 0.)
@@ -390,6 +445,7 @@ class ROI_PAC(SceneIO):
                           'Theta and phi are defaulting to vertical incident!')
 
         c['meta']['title'] = par.get('TITLE', 'None')
+        c['meta']['wavelength'] = par['WAVELENGTH']
         c['bin_file'] = filename
         c['par_file'] = par_file
 
@@ -451,7 +507,7 @@ class ISCE(SceneIO):
         rdr_files = glob.glob(os.path.join(path, '*.rdr.geo'))
 
         if len(rdr_files) == 0:
-            raise ImportError('Could not find LOS file (.rdr.geo)')
+            raise ImportError('Could not find LOS file (*.rdr.geo)')
         return rdr_files[0]
 
     @staticmethod
@@ -486,11 +542,11 @@ class ISCE(SceneIO):
             (nlat * coord_lat['delta'])
         c['frame']['llLon'] = coord_lon['startingvalue']
 
-        displacement = num.memmap(self._getDisplacementFile(path),
-                                  dtype='<f4')\
+        displ = num.memmap(self._getDisplacementFile(path),
+                           dtype='<f4')\
             .reshape(nlat, nlon*2)[:, nlon:]
-        displacement[displacement == 0.] = num.nan
-        c['displacement'] = displacement*1e-2
+        displ[displ == 0.] = num.nan
+        c['displacement'] = displ
 
         los_data = num.fromfile(self._getLOSFile(path), dtype='<f4')\
             .reshape(nlat, nlon*2)
@@ -552,17 +608,18 @@ class GMTSAR(SceneIO):
 
         grd = netcdf.netcdf_file(self._getDisplacementFile(path),
                                  mode='r', version=2)
-        c['displacement'] = grd.variables['z'][:].copy()
-        c['displacement'] *= 1e-2
+        displ = grd.variables['z'][:].copy()
+        displ /= 1e2  # los_ll.grd files come in cm
+        c['displacement'] = displ
         shape = c['displacement'].shape
         # LatLon
         c['frame']['llLat'] = grd.variables['lat'][:].min()
         c['frame']['llLon'] = grd.variables['lon'][:].min()
 
         c['frame']['dLat'] = (grd.variables['lat'][:].max() -
-                              c['frame']['llLat'])/shape[1]
+                              c['frame']['llLat'])/shape[0]
         c['frame']['dLon'] = (grd.variables['lon'][:].max() -
-                              c['frame']['llLon'])/shape[0]
+                              c['frame']['llLon'])/shape[1]
 
         # Theta and Phi
         try:
