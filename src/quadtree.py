3de0c3c37cc7c754c8621ddbd5c17c13767e6c0e
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2018-05-02 09:44:36 +0200

    add fallback for numpy versions lacking nanmedian

--- src/quadtree.py
+++ src/quadtree.py
@@ -5,6 +5,14 @@ from pyrocko import guts
 from .util import Subject, property_cached, derampMatrix
 
 
+try:
+    nanmedian = num.nanmedian
+
+except AttributeError:
+    def nanmedian(x, **kwargs):
+        return num.median(x[num.isfinite(x)], **kwargs)
+
+
 class QuadNode(object):
     ''' A node (or *tile*) in held by :class:`~kite.Quadtree`. Each node in the
     tree hold a back reference to the quadtree and scene to access
@@ -61,7 +69,7 @@ class QuadNode(object):
         ''' Median displacement
         :type: float
         '''
-        return float(num.nanmedian(self.displacement))
+        return float(nanmedian(self.displacement))
 
     @property_cached
     def std(self):
@@ -152,7 +160,7 @@ class QuadNode(object):
         :type: float
         '''
         phi = self.scene.phi[self._slice_rows, self._slice_cols]
-        return num.nanmedian(phi[~self.displacement_mask])
+        return nanmedian(phi[~self.displacement_mask])
 
     @property_cached
     def theta(self):
@@ -160,7 +168,7 @@ class QuadNode(object):
         :type: float
         '''
         theta = self.scene.theta[self._slice_rows, self._slice_cols]
-        return num.nanmedian(theta[~self.displacement_mask])
+        return nanmedian(theta[~self.displacement_mask])
 
     @property_cached
     def gridE(self):
