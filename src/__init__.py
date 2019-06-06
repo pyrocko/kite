f721cceb168e0bc8807b6116687412fd302180bb
miili <isken@gfz-potsdam.de>
2019-06-06 14:45:52 +0200

    version: using pkg_resources

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+import pkg_resources
+
 from .scene import Scene, TestScene, read  # noqa
 from .quadtree import Quadtree  # noqa
 from .covariance import Covariance  # noqa
 from .sandbox_scene import SandboxScene, TestSandboxScene  # noqa
 
-
-__version__ = '1.1.0'
+__version__ = pkg_resources.get_distribution('kite').version
